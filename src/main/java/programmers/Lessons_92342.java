5f0bc5d7dcc7d7f3de52965474bd0f8bfeb96bc1
jykim <wodusl1103@naver.com>
2022-08-04 14:12:25 +0900

    refactor: programmers 코딩테스트 연습 > 2022 KAKAO BLIND RECRUITMENT > 양궁대회
    https://school.programmers.co.kr/learn/courses/30/lessons/92342
    시간 감소

--- src/main/java/programmers/Lessons_92342.java
+++ src/main/java/programmers/Lessons_92342.java
@@ -1,52 +1,39 @@
 package programmers;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * https://school.programmers.co.kr/learn/courses/30/lessons/92342
  * 코딩테스트 연습 > 2022 KAKAO BLIND RECRUITMENT > 양궁대회
  * */
 public class Lessons_92342 {
     private int[] givenInfo;
-    private int gapMax = 1;
-    private final List<int[]> list = new ArrayList<>();
+    private int gapMax = 0;
+    private int[] answer = {-1};
 
     public int[] solution(int n, int[] info) {
         givenInfo = info;
-        dfs(n, new int[11], 0);
-        return list.stream().max((o1, o2) -> {
-            for (int i = 10; i >=0; i --) {
-                if (o1[i] == o2[i]) continue;
-                return o1[i] - o2[i];
-            }
-            return 0;
-        }).orElse(new int[] {-1});
+        dfs(n, new int[11], 9);
+        return answer;
     }
 
     private void dfs(int count, int[] result, int start) {
-        if (count == 0 || start == 10) {
+        if (count == 0 || start == -1) {
             int gap = getScoreGap(result);
-            if (gapMax <= gap) {
-                if (gapMax < gap) {
-                    list.clear();
-                }
+            if (gapMax < gap) {
                 gapMax = gap;
-                int[] tmp = result.clone();
-                tmp[10] += count;
-                list.add(tmp);
+                answer = result.clone();
+                answer[10] += count;
             }
             return;
         }
-        for (int i = start; i < 10; i ++) {
+        for (int i = start; i >= 0; i --) {
             int nextCount = count - givenInfo[i] - 1;
             if (nextCount >= 0) {
                 result[i] += (givenInfo[i] + 1);
-                dfs(nextCount, result, i + 1);
+                dfs(nextCount, result, i - 1);
                 result[i] -= (givenInfo[i] + 1);
                 continue;
             }
-            dfs(count, result, i + 1);
+            dfs(count, result, i - 1);
         }
     }
 
