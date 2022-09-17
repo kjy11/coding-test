e975e7b600016f81515e3549a43849b96a1fbb5d
jykim <wodusl1103@naver.com>
2022-09-17 18:04:05 +0900

    add: programmers 코딩테스트 연습 > 힙(Heap) > 이중우선순위큐
    https://school.programmers.co.kr/learn/courses/30/lessons/42628
    
    - TreeMap을 사용하여 풀이 -> 효율성 개선

--- src/main/java/programmers/Lessons_42628.java
+++ src/main/java/programmers/Lessons_42628.java
@@ -4,6 +4,7 @@ package programmers;
 import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.TreeMap;
 
 /**
  * https://school.programmers.co.kr/learn/courses/30/lessons/42628
@@ -11,6 +12,48 @@ import java.util.Queue;
  * */
 public class Lessons_42628 {
     public int[] solution(String[] operations) {
+        int[] answer = {0, 0};
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (String operation : operations) {
+            String[] arr = operation.split(" ");
+            String opString = arr[0];
+            int opInt = Integer.parseInt(arr[1]);
+
+            if (opString.equals("I")) {
+                if (map.containsKey(opInt)) {
+                    map.put(opInt, map.get(opInt) + 1);
+                } else {
+                    map.put(opInt, 1);
+                }
+                continue;
+            }
+
+            if (opString.equals("D") && map.isEmpty()) {
+                continue;
+            }
+
+            Integer key = null;
+            if (opString.equals("D") && opInt == 1) {
+                key = map.lastKey();
+            }
+            if (opString.equals("D") && opInt == -1) {
+                key = map.firstKey();
+            }
+            if (key != null) {
+                Integer count = map.put(key, map.get(key) - 1);
+                if (count != null && count == 1) {
+                    map.remove(key);
+                }
+            }
+        }
+        if (!map.isEmpty()) {
+            answer[0] = map.lastKey();
+            answer[1] = map.firstKey();
+        }
+        return answer;
+    }
+
+    public int[] solution1(String[] operations) {
         int[] answer = {0, 0};
         Queue<Integer> queue = new PriorityQueue<>();
         Queue<Integer> queueReverse = new PriorityQueue<>(Collections.reverseOrder());
